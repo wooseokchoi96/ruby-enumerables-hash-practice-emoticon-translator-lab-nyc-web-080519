663ca0373f4a8703f9e3625f1e91cef19d53a5e9
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 21:37:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,9 @@ def load_library(file_name)
   translator = {get_meaning: {}, get_emoticon: {}}
   library.each{ |meaning, emotes|
     translator[:get_meaning][emotes[1]] = meaning
-    translator[:get_emoticon][emotes[0]] = 
-    
+    translator[:get_emoticon][emotes[0]] = emotes[1]
   }
+  translator
 end
 
 def get_japanese_emoticon
